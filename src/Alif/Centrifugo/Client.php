08f4e666b2e86ddd72beea3803d5f9d87181823a
Mustafo Zubaydulloev <mustafo.zubaydulloev@gmail.com>
2017-10-03 15:57:02 +0500

    Ppublish

--- src/Alif/Centrifugo/Client.php
+++ src/Alif/Centrifugo/Client.php
@@ -4,6 +4,24 @@ namespace Alif\Centrifugo;
 
 class Client extends \phpcent\Client
 {
+
+    /**
+     * Send message into channel of namespace. data is an actual information you want to send into channel
+     *
+     * @param       $channel
+     * @param array $data
+     * @return mixed
+     */
+    public function publish($channel, $data = [])
+    {
+        $result = parent::publish($channel, $data);
+
+        if (function_exists('event'))
+            event(CentrifugoMessagePublished::class, $data);
+
+        return $result;
+    }
+
     /**
      * Generate common client token
      *
